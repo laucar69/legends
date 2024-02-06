7f33a676157ea125dc6fc534c0de86a0e6100ccf
Carsten Lau <carsten.lau@coop.ch>
2024-02-06 10:59:06 +0100

    ok

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <div className="d-flex m-2 justify-content-center fixed-bottom text-secondary bg-light">
-       <p>🥡 Legends | 📫 legends@legends.de | 📞 0176 237 680 37 | ⏰ Mo - So 17 - 24 Uhr</p>
+       <p>🥡 Legends | 📫 info@legends.de | 📞 0176 237 680 37 | ⏰ Mo - So 17 - 24 Uhr</p>
       </div>
     )
   }
